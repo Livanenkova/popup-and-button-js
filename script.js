adb9a6802a1f580e72469692c7b7f39b67953c20
Livanenkova Arina <arina909@yandex.ru>
2019-03-23 11:40:35 +0300

    new version

---
+++ script.js
@@ -0,0 +1,4 @@
+
+var kr2 = document.getElementById("k2");
+kr2.addEventListener("click",function(change)';
+change = document.getElementById('feedback').innerHTML='<P><font  color=red>Крученых молодец</font>';
