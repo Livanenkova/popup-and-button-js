885b1b19f0aa96f5f864bd13890797c021c5f2b4
Livanenkova Arina <arina909@yandex.ru>
2019-03-25 16:25:47 +0300

    new version

--- script.js
+++ script.js
@@ -1,4 +1,82 @@
+// [
+//   {key: 'kruchenih', value: 'this is about c'},
+//   {key: 'm', value: 'this is about m'},
+//   {key: 'b', value: 'this is about b'},
+//   {key: 'a', value: 'this is about ARINA!!!!111'}
+// ].forEach(({key, value}) => this[key].onclick = () => {
+//   this.wrapper.style.display = 'flex';
+//   document.getElementById('content').innerHTML = value;
+// })
 
-var kr2 = document.getElementById("k2");
-kr2.addEventListener("click",function(change)';
-change = document.getElementById('feedback').innerHTML='<P><font  color=red>Крученых молодец</font>';
+// this.closePopup.onclick = () => {
+//   this.wrapper.style.display = 'none';
+// }
+
+var kruchenihButton = document.getElementById('kruchenih');
+var popupWrapper = document.getElementById('wrapper');
+var popupContent = document.getElementById('content');
+var closePopupButton = document.getElementById('closePopup');
+
+function openKruchenihPopup() {
+  popupWrapper.style.display = 'flex';
+  popupContent.innerHTML = 'крученых крут';
+};
+
+function closePopup() {
+  popupWrapper.style.display = 'none';
+};
+
+kruchenihButton.onclick = openKruchenihPopup;
+closePopupButton.onclick = closePopup;
+
+var mayakovskiiButton = document.getElementById('mayakovskii');
+var popupWrapper = document.getElementById('wrapper');
+var popupContent = document.getElementById('content');
+var closePopupButton = document.getElementById('closePopup');
+
+function openMayakovskiiPopup() {
+  popupWrapper.style.display = 'flex';
+  popupContent.innerHTML = 'Маяк Молодец';
+};
+
+function closePopup() {
+  popupWrapper.style.display = 'none';
+};
+
+mayakovskiiButton.onclick = openMayakovskiiPopup;
+closePopupButton.onclick = closePopup;
+
+var burlukButton = document.getElementById('burluk');
+var popupWrapper = document.getElementById('wrapper');
+var popupContent = document.getElementById('content');
+var closePopupButton = document.getElementById('closePopup');
+
+function openBurlukPopup() {
+  popupWrapper.style.display = 'flex';
+  popupContent.innerHTML = 'Бурлюк бодрый';
+};
+
+function closePopup() {
+  popupWrapper.style.display = 'none';
+};
+
+burlukButton.onclick = openBurlukPopup;
+closePopupButton.onclick = closePopup;
+
+var arinaButton = document.getElementById('arina');
+var popupWrapper = document.getElementById('wrapper');
+var popupContent = document.getElementById('content');
+var closePopupButton = document.getElementById('closePopup');
+
+function openArinaPopup() {
+  popupWrapper.style.display = 'flex';
+  popupContent.innerHTML = 'Арина Арина Арина';
+};
+
+function closePopup() {
+  popupWrapper.style.display = 'none';
+};
+
+arinaButton.onclick = openArinaPopup;
+closePopupButton.onclick = closePopup;
+
